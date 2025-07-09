618c3c4324ed3dc0dda58a200e887c5b3df0637f
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2025-07-09 22:40:06 +0530

    new logo

--- components/commons/navbar.tsx
+++ components/commons/navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
       <div className="max-w-6xl mx-auto w-full flex items-center justify-between px-8">
         <div className="flex items-center gap-10">
           <Link href="/" className="text-sm tracking-wide flex items-center gap-2 font-ropa text-white">
-            <img src="/assets/logo_crates.svg" alt="Use Crates Logo" className="h-10 w-auto" />
+            <img src="/assets/crates.png" alt="Use Crates Logo" className="h-10 w-auto" />
             <span className="text-white text-xl font-chakra">Crates</span>
           </Link>
           {/* <nav className="flex items-center gap-8 text-sm font-ropa">
